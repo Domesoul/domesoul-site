1224c63135fc491fd88dcaa2fc2a608c2435ee72
Domesoul <udoekongud@gmail.com>
2025-07-19 22:18:30 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
-
 let slideIndex = 0;
 showSlides();
 function showSlides() {
-  const slides = document.getElementsByClassName("slide");
-  for (let i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
+  let slides = document.getElementsByClassName("carousel");
+  for (let i = 0; i < slides.length; i++) { slides[i].style.display = "none"; }
   slideIndex++;
   if (slideIndex > slides.length) { slideIndex = 1; }
   slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 4000);
+  setTimeout(showSlides, 5000); // Change image every 5 seconds
 }
