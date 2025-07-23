3eb4283e9e7bd7e27df587b36461e096bcb87acb
Domesoul <udoekongud@gmail.com>
2025-07-23 17:45:18 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -28,3 +28,18 @@ document.addEventListener("DOMContentLoaded", function () {
     `;
   }, 1000);
 });
+
+function openModal(songId) {
+  document.getElementById('modal-' + songId).style.display = "block";
+}
+function closeModal(songId) {
+  document.getElementById('modal-' + songId).style.display = "none";
+}
+window.onclick = function(event) {
+  const modals = document.querySelectorAll(".modal");
+  modals.forEach(modal => {
+    if (event.target == modal) {
+      modal.style.display = "none";
+    }
+  });
+}
