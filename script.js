af10b004c98691695632a3009c5cd4df4fd7808b
Domesoul <udoekongud@gmail.com>
2025-07-26 17:42:27 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
 console.log('Domé Soul site loaded');
+const toggleBtn = document.querySelector('.toggle-button');
+const navLinks = document.querySelector('.nav-links');
+
+toggleBtn.addEventListener('click', () => {
+  navLinks.classList.toggle('active');
+});
