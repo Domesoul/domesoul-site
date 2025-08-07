202b16a4947764cca39448d7e267e0059abe69c8
Domesoul <udoekongud@gmail.com>
2025-08-07 17:07:58 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,18 +1,4 @@
+
 function openModal(id) {
-  const modal = document.getElementById(id);
-  if (modal) modal.style.display = "block";
+  alert('Open modal for: ' + id);
 }
-
-function closeModal(id) {
-  const modal = document.getElementById(id);
-  if (modal) modal.style.display = "none";
-}
-
-window.onclick = function(event) {
-  const modals = document.querySelectorAll('.modal');
-  modals.forEach(modal => {
-    if (event.target === modal) {
-      modal.style.display = "none";
-    }
-  });
-};
